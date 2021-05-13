e26156edf9b290b27695dd1f3420f9e4f100a008
Tsubasa <J16N@users.noreply.github.com>
2021-05-13 22:20:16 +0530

    Added solution of part 2

---
+++ Assignment-4/p10b.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("\n------ HOLLOW DIAMOND ------\n");
+
+  int rows = 0;
+  printf("\n Rows: "); scanf("%d", &rows);
+
+  int max_star = 2 * rows - 1;
+
+  for (int i = 0, j = 0, k = 0, l = 0; i < max_star;)
+  {
+    printf("%s%*s %s%s", !i ? "\n" : "", !j ? 2 * ((max_star / 2 + 1) - l) : 0, 
+      "", j > 0 && j < k ? " " : "*", !k && i + 1 == max_star ? "\n" : "");
+
+    if (j == k)
+    {
+      printf("\n");
+      ++i;
+      j = 0;
+      k = i < rows ? k + 2 : k - 2;
+      l = i < rows ? l + 1 : l - 1;
+    }
+    else ++j;
+  }
+
+  return 0;
+}
