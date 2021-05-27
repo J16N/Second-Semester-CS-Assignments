0a0791a8d68e9163e36957a01712d72375d549fd
Tsubasa <J16N@users.noreply.github.com>
2021-05-27 06:33:20 +0000

    Added solution to problem 1

---
+++ Assignment-6/p1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+  printf("\n------ PATTERN - I ------\n");
+  
+  int rows = 0;
+  printf(" Rows: "); scanf("%d", &rows);
+
+  int space = log10(rows) + 3;
+
+  for (int i = 0; i < rows; ++i)
+  {
+    if (!i) printf("\n");
+
+    for (int j = 0; j < rows; ++j)
+    {
+      if (!j || j == rows - 1 || !i || i == rows - 1)
+        printf(" %*s%*d", j == rows - 1 ? 2 : 0, "", j == rows - 1 ? -space : space, i + 1);
+      else printf(" %*s%*s", j == rows - 1 ? 2 : 0, "", space, " ");
+    }
+
+    printf("%s", i == rows - 1 ? "\n\n" : "\n");
+  }
+
+  return 0;
+}
